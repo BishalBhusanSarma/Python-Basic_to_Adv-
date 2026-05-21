63686c9bb881b38d08fafe0292589a1a55d89ac5
Bishal Bhusan Sarma <bishalbhusansarma87787@gmail.com>
2026-05-21 11:38:40 +0530

    updated

--- files/prac_Q/check_if_element_present/json_to _py.py
+++ files/prac_Q/check_if_element_present/json_to _py.py
@@ -3,4 +3,9 @@ import json
 x = '{"name": "Thanos", "age": "567", "PH": 8236478648746, "height": 8.5}'
 y = json.loads(x)
 
-assert "Thanddds" in json.dumps(y)
+try:
+    assert "age" in json.dumps(y)
+    print("found")
+
+except:
+    print("not found")
